38cf30e9d0ff50b657caf9ecf78f3d6a0d0425fe
kayck45 <uzumakikayck@hotmail.com>
2023-09-20 18:37:44 -0300

    k

---
+++ cola/colaK.js
@@ -0,0 +1,3 @@
+alert // representa um alerta quando abre a pagina
+confirm // cria uma caixa para proceguir quando abre a pagina (confirmação) 
+prompt // cria uma caixa de digitação quando abre a pagina (confimação) 
